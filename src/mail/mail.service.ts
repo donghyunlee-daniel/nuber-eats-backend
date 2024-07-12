f5448541e7aec140638eac7fbce602958e2f99e3
donghyunlee-daniel <d.lee77668@gmail.com>
2024-07-11 23:15:04 -0400

    Done with Unit test for Jwt.service, mail.service and user.service

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,11 +10,11 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {}
 
-  private async sendEmail(
+  async sendEmail(
     subject: string,
     template: string,
     emailVars: EmailVar[],
-  ) {
+  ): Promise<boolean> {
     const form = new FormData();
     form.append(
       'from',
@@ -25,8 +25,7 @@ export class MailService {
     form.append('template', template);
     emailVars.forEach((eVar) => form.append(`v:${eVar.key}`, eVar.value));
     try {
-      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
-        method: 'POST',
+      await got.post(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
         headers: {
           Authorization: `Basic ${Buffer.from(
             `api:${this.options.apiKey}`,
@@ -34,8 +33,9 @@ export class MailService {
         },
         body: form,
       });
+      return true;
     } catch (error) {
-      console.log(error);
+      return false;
     }
   }
 
