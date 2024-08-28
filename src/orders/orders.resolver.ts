56c87c293a1a7aa20c6f973be68eb59551724551
donghyunlee-daniel <d.lee77668@gmail.com>
2024-08-28 19:48:55 -0400

    Order CRU is done. Driver Resolver will be done after subscription because the driver is assigned when the food is ready while they are subscribing the events

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,49 +1,51 @@
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { Order } from "./entities/order.entity";
-import { OrderService } from "./orders.service";
-import { CreateOrderInput, CreateOrderOutput } from "./dtos/create-order.dto";
-import { AuthUser } from "src/auth/auth-user.decorator";
-import { User } from "src/users/entities/user.entity";
-import { Role } from "src/auth/role.decorator";
-import { GetOrdersInput, GetOrdersOutPut } from "./dtos/get-orders.dto";
-import { GetOrderInput, GetOrderOutput } from "./dtos/get-order.dto";
-import { EditOrderInput, EditOrderOutput } from "./dtos/edit-order.dto";
-
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Order } from './entities/order.entity';
+import { OrderService } from './orders.service';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { Role } from 'src/auth/role.decorator';
+import { GetOrdersInput, GetOrdersOutPut } from './dtos/get-orders.dto';
+import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
+import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
 
 @Resolver((of) => Order)
 export class OrderResolver {
   constructor(private readonly ordersService: OrderService) {}
 
   @Mutation((returns) => CreateOrderOutput)
-  @Role(["Client"])
+  @Role(['Client'])
   async createOrder(
     @AuthUser() customer: User,
     @Args('input') createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
-    return this.ordersService.createOrder(customer, createOrderInput)
+    return this.ordersService.createOrder(customer, createOrderInput);
   }
 
-  @Query(returns => GetOrdersOutPut)
+  @Query((returns) => GetOrdersOutPut)
   @Role(['Any'])
   async getOrders(
     @AuthUser() user: User,
-    @Args('input') getOrdersInput: GetOrdersInput
-  ): Promise<GetOrdersOutPut>{
-    return this.ordersService.getOrders(user, getOrdersInput)
+    @Args('input') getOrdersInput: GetOrdersInput,
+  ): Promise<GetOrdersOutPut> {
+    return this.ordersService.getOrders(user, getOrdersInput);
   }
 
-  @Query(returns => GetOrderOutput)
+  @Query((returns) => GetOrderOutput)
   @Role(['Any'])
   async getOrder(
     @AuthUser() user: User,
-    @Args('input') getOrderInput: GetOrderInput
-  ): Promise<GetOrderOutput>{
-    return this.ordersService.getOrder(user, getOrderInput)
+    @Args('input') getOrderInput: GetOrderInput,
+  ): Promise<GetOrderOutput> {
+    return this.ordersService.getOrder(user, getOrderInput);
   }
 
-  @Mutation(returns => EditOrderOutput)
+  @Mutation((returns) => EditOrderOutput)
   @Role(['Any'])
-  async editOrder(@AuthUser() user:User, editOrderInput: EditOrderInput):Promise<EditOrderOutput>{
-    return this.ordersService.editOrder(user,editOrderInput)
+  async editOrder(
+    @AuthUser() user: User,
+    @Args('input') editOrderInput: EditOrderInput,
+  ): Promise<EditOrderOutput> {
+    return this.ordersService.editOrder(user, editOrderInput);
   }
 }
