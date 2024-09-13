68e878d7d4afb613e9af56aad4929d65dc8af48f
donghyunlee-daniel <d.lee77668@gmail.com>
2024-09-12 20:57:08 -0400

    Modified some codes to have only one 'pubsub' for this application. Evertying is working fine after changing codes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,6 +23,7 @@ import { Dish } from './restaurant/entities/dish.entity';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -93,6 +94,7 @@ import { OrderItem } from './orders/entities/order-item.entity';
     }),
     RestaurantModule,
     OrdersModule,
+    CommonModule
   ],
   controllers: [],
   providers: [],
--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -8,13 +8,16 @@ import { Role } from 'src/auth/role.decorator';
 import { GetOrdersInput, GetOrdersOutPut } from './dtos/get-orders.dto';
 import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
+import { Inject } from '@nestjs/common';
+import { PUB_SUB } from 'src/common/common.constants';
 import { PubSub } from 'graphql-subscriptions';
-
-const pubsub = new PubSub();
 
 @Resolver((of) => Order)
 export class OrderResolver {
-  constructor(private readonly ordersService: OrderService) {}
+  constructor(
+    private readonly ordersService: OrderService,
+    @Inject(PUB_SUB) private readonly pubSub: PubSub,
+  ) {}
 
   @Mutation((returns) => CreateOrderOutput)
   @Role(['Client'])
@@ -50,11 +53,11 @@ export class OrderResolver {
     @Args('input') editOrderInput: EditOrderInput,
   ): Promise<EditOrderOutput> {
     return this.ordersService.editOrder(user, editOrderInput);
-  } 
+  }
 
   @Mutation((returns) => Boolean)
   potatoReady() {
-    pubsub.publish('hotPotatoes', { readyPotatos: 'Your Potato is ready.' });
+    this.pubSub.publish('hotPotatoes', { readyPotatos: 'Your Potato is ready.' });
     return true;
   }
 
@@ -62,6 +65,6 @@ export class OrderResolver {
   @Role(['Any'])
   readyPotatos(@AuthUser() user: User) {
     console.log(user);
-    return pubsub.asyncIterator('hotPotatoes');
+    return this.pubSub.asyncIterator('hotPotatoes');
   }
 }
